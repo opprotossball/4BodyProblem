8012c3dcb74fc3f2c18b610786fb84b3aedf24cb
Jan Stachurski <jan.stachurski@outlook.com>
2024-12-07 20:03:33 +0100

    cors

--- back/main.py
+++ back/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from routers import auth, websockets, astronomy
 from fastapi.routing import APIRouter
 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,  # Allow credentials
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all HTTP headers
+)
+
 router = APIRouter(prefix="/api/v1")
 router.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(astronomy.router, prefix="/astronomy", tags=["astronomy"])
