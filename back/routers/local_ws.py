98df660c1bfb39bf23e342708f20781b8fba1282
Jan Stachurski <jan.stachurski@outlook.com>
2024-12-08 05:12:31 +0100

    gemini integration

--- back/routers/local_ws.py
+++ back/routers/local_ws.py
@@ -5,6 +5,11 @@ import os
 from dotenv import load_dotenv
 from time import sleep
 import websockets
+import json
+
+from routers.inter_ws import send
+from llm import predict_responses
+import json
 
 frontend_ws_connections = {}
 
@@ -16,10 +21,6 @@ async def open_local_ws(user_id: str):
         raise HTTPException(status_code=400, detail="WebSocket already open for this user")
     return InitiateWebSocketResponse(websocket_url=f"ws://localhost:8000/api/v1/ws/{user_id}")
 
-# @router.post("/interplanetary-ws", response_model=InitiateWebSocketResponse)
-# async def open_websocket():
-#     return InitiateWebSocketResponse(websocket_url=f"ws://localhost:8000/api/v1/ws/interplanetary")
-
 @router.websocket("/ws/{user_id}")
 async def websocket_endpoint(websocket: WebSocket, user_id: str):
     await websocket.accept()
@@ -27,7 +28,29 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str):
     try:
         while True:
             data = await websocket.receive_text()
-            #await websocket.send_text(f"Received message: {data}")
+            print(f"received local: {data}")
+            await handle_local_message(json.loads(data))
     except WebSocketDisconnect:
         del frontend_ws_connections[user_id]
         print(f"WebSocket disconnected for user {user_id}")
+
+async def send_to_all(message: str):
+    # Iterate over all WebSocket connections
+    for user_id, websocket in frontend_ws_connections.items():
+        try:
+            await websocket.send_text(message)
+            print(f"Message sent to user {user_id}: {message}")
+        except Exception as e:
+            print(f"Failed to send message to user {user_id}: {e}")
+
+async def handle_local_message(message):
+    print("handling message")
+    send(message)
+    predictions = predict_responses(message["message"], 3)
+    print(predictions)
+    for pred in predictions:
+        response = {
+            "message": pred,
+            "is_predicted": True
+        }
+        await send_to_all(json.dumps(response))
